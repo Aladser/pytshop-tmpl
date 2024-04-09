72c98cac9d62959704e450cad5d058ce334cfe3c
Aladser <aladser@gmail.com>
2024-04-10 07:40:40 +0900

    обновлены тесты Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,19 +3,35 @@ from classes import Product, Smartphone, Grass
 
 
 @pytest.fixture
-def product():
-    return Product(name='Хлеб', description="товар", price=10, quantity=5)
+def prd_params():
+    return {
+        'name': 'Хлеб',
+        'description': "товар",
+        'price': 10,
+        'quantity': 5
+    }
 
 
-def test_init(product):
-    assert product.name == 'Хлеб'
-    assert product.price == 10
-    assert product.quantity == 5
-    assert len(product) == 5
-    with pytest.raises(Exception):
-        assert product.quantity == -5
-    assert product.description == "товар"
+@pytest.fixture
+def product(prd_params):
+    return Product(**prd_params)
+
+
+def test_init(product, prd_params):
+    prd = Product(**prd_params)
+    assert prd.name == prd_params['name']
+    assert product.price == prd_params['price']
+    assert product.quantity == prd_params['quantity']
+    assert product.description == prd_params['description']
     print(f"\n{product}", end='')
+
+
+def test_create(product, prd_params):
+    prd = Product.create(prd_params)
+    assert prd.name == prd_params['name']
+    assert product.price == prd_params['price']
+    assert product.quantity == prd_params['quantity']
+    assert product.description == prd_params['description']
 
 
 def test_price(product):
@@ -26,18 +42,6 @@ def test_price(product):
     # нулевая цена
     with pytest.raises(Exception):
         product.price = 0
-
-
-def test_create(product):
-    title = 'яблоко'
-    price = 11.5
-    count = 3
-    description = 'фрукт'
-    product = Product.create({'name': title, 'price': price, 'quantity': count, 'description': description})
-    assert product.name == title
-    assert product.price == price
-    assert product.quantity == count
-    assert product.description == description
 
 
 def test_add(product):
