118121f4f1a491f06cc773c0078cd0f28471bc3f
Aladser <aladser@gmail.com>
2024-04-10 20:08:23 +0900

    обновлен BaseProduct

--- classes/product/base_product.py
+++ classes/product/base_product.py
@@ -6,3 +6,26 @@ class BaseProduct(ABC):
     @abstractmethod
     def create(cls, prd_obj: dict):
         pass
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def price(self) -> float:
+        pass
+
+    @abstractmethod
+    def __add__(self, other) -> float:
+        pass
+
+    @abstractmethod
+    def __len__(self):
+        pass
